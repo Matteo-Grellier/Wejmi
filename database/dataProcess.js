b59cf462460af331e376b98f1d55b737f9f75b94
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-04-11 16:57:32 +0200

    [BDD.js, dataProcess.js] added all the functions to use the DB

--- database/dataProcess.js
+++ database/dataProcess.js
@@ -2,7 +2,7 @@ import { Asset } from "expo-asset"
 import * as FileSystem from "expo-file-system"
 import * as SQLite from "expo-sqlite"
 
-export default async function openDatabase() {
+export async function openDatabase() {
     if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
         await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
     }
@@ -13,4 +13,128 @@ export default async function openDatabase() {
         );
     }
     return SQLite.openDatabase('wejmiDatabase.db');
+}
+
+// -------------- Objects --------------
+
+export async function GetAllObject() {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("SELECT * FROM object",[], (_, {insertID, rows}) => {
+            console.log(rows);
+            return rows
+        });
+    })
+
+}
+
+export async function AddObject(name, id_room, id_category, id_furniture, photo_uri) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("INSERT INTO object (name, id_room, id_category, id_furniture, id_state, photo_uri) VALUES (?, ?, ?, ?, 1, ?)", [name, id_room, id_category, id_furniture, photo_uri] );
+    })
+
+}
+
+export async function ModifyObject(id, name, id_room, id_category, id_furniture, state, photo_uri) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("UPDATE object SET name = ?, id_room = ?, id_category = ?, id_furniture = ?, id_state = ?, photo_uri = ? WHERE id = ?", [name, id_room, id_category, id_furniture, state, photo_uri, id] );
+    })
+
+}
+
+export async function DeleteObject(id) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("DELETE FROM object WHERE id = ?;", [id] );
+    })
+
+}
+
+// -------------- Categories --------------
+
+export async function GetAllCategory (name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("SELECT * FROM category",[], (_, {insertID, rows}) => {
+            console.log(rows);
+            return rows
+        });
+    })
+
+}
+
+export async function AddCategory(name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("INSERT INTO category (name) VALUES (?)", [name] );
+    })
+
+}
+
+export async function DeleteCategory(id) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("DELETE FROM category WHERE id = ?;", [id] );
+    })
+
+}
+
+// -------------- Rooms --------------
+
+export async function GetAllRoom (name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("SELECT * FROM room",[], (_, {insertID, rows}) => {
+            console.log(rows);
+            return rows
+        });
+    })
+
+}
+
+export async function AddRoom(name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("INSERT INTO room (name) VALUES (?)", [name] );
+    })
+
+}
+
+export async function DeleteRoom(id) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("DELETE FROM room WHERE id = ?;", [id] );
+    })
+
+}
+
+// -------------- Furniture --------------
+
+export async function GetAllFurniture (name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("SELECT * FROM furniture",[], (_, {insertID, rows}) => {
+            console.log(rows);
+            return rows
+        });
+    })
+
+}
+
+export async function AddFurniture(name) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("INSERT INTO furniture (name) VALUES (?)", [name] );
+    })
+
+}
+
+export async function DeleteFurniture(id) {
+    const db = await openDatabase();
+    db.transaction((tx) => {
+        tx.executeSql("DELETE FROM furniture WHERE id = ?;", [id] );
+    })
+
 }
--- screens/BDD.js
+++ screens/BDD.js
@@ -1,4 +1,4 @@
-import  openDatabase  from "../database/dataProcess.js";
+import { openDatabase, GetAllObject, AddObject, ModifyObject, DeleteObject, GetAllCategory, AddCategory, DeleteCategory, GetAllRoom, AddRoom, DeleteRoom, GetAllFurniture, AddFurniture, DeleteFurniture } from "../database/dataProcess.js";
 import { Image } from "native-base";
 import { useState } from "react";
 import { View, Text, Button } from "react-native";
@@ -8,13 +8,11 @@ export default () => {
     const [texty, setTexty] = useState("data yeager ! data yeager ! na nana, data yeager. data yeager ! data yeager ! na nana nanana NA NA NA !");
 
     const DataYe = async () => {
-        console.log("bouton");
-        const db = await openDatabase();
-        db.transaction((tx) => {
-            tx.executeSql("SELECT * FROM category",[], (_, {insertID, rows}) => {
-                console.log(rows);
-            });
-        })
+        // console.log("bouton");
+
+        // DeleteFurniture(4);
+
+        // const bob = GetAllFurniture();
     };
 
     return (
